898c6351efed4bb05ac96477aeaac599e086383b
Tomato6966 <chris.pre03@gmail.com>
2023-10-26 12:57:46 +0200

    fix scope handling (#6)
    * Fix scope handling

--- src/index.ts
+++ src/index.ts
@@ -218,7 +218,7 @@ export class DiscordStrategy<User> extends OAuth2Strategy<
 > {
   name = DiscordStrategyDefaultName;
 
-  private scope: Array<DiscordScope>;
+  private scope: string;
   private prompt?: "none" | "consent";
   private userInfoURL = `${discordApiBaseURL}/users/@me`;
 
@@ -246,14 +246,14 @@ export class DiscordStrategy<User> extends OAuth2Strategy<
       verify
     );
 
-    this.scope = scope ? scope : ["identify", "email"];
+    this.scope = (scope ? scope : ["identify", "email"]).join(" ");
 
     this.prompt = prompt;
   }
 
   protected authorizationParams() {
     const params = new URLSearchParams({
-      scope: this.scope.join(" "),
+      scope: this.scope,
     });
     if (this.prompt) params.set("prompt", this.prompt);
     return params;
